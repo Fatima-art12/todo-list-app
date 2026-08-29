9345aaea9cc024a2e9f751bca76cf34b71403bf6
Fatima <aboulfatima52@gmail.com>
2026-08-29 22:16:46 +0500

    Fixed dark mode filter button text color

--- script.js
+++ script.js
@@ -241,3 +241,23 @@ menuProfileBtn.addEventListener("click", function() {
 });
 
 loadProfile();
+const darkModeToggle = document.getElementById("darkModeToggle");
+
+let isDarkMode = localStorage.getItem("darkMode") === "true";
+
+if (isDarkMode) {
+    document.body.classList.add("dark-mode");
+    darkModeToggle.checked = true;
+}
+
+darkModeToggle.addEventListener("change", function() {
+
+    if (darkModeToggle.checked) {
+        document.body.classList.add("dark-mode");
+        localStorage.setItem("darkMode", "true");
+    } else {
+        document.body.classList.remove("dark-mode");
+        localStorage.setItem("darkMode", "false");
+    }
+
+});
